6fb8c5ab7dd57fe185296748e748702a7bb0855c
gberliner <guy.berliner@papercut.com>
2019-04-11 19:28:42 -0700

    rip out broken gulp-sass garbage

--- tasks/build.js
+++ tasks/build.js
@@ -3,14 +3,14 @@ import './clean'
 import './manifest'
 import './scripts'
 import './pack'
-// import './styles'
-// import './pages'
-// import './locales'
-// import './images'
+import './styles'
+import './pages'
+import './locales'
+import './images'
 // import './fonts'
 // import './chromereload'
 
-gulp.task('build', gulp.series('clean','manifest','scripts','pack'))//,gulp.parallel(   'manifest')))//,
+gulp.task('build', gulp.series('clean','manifest','pages','images','styles','locales','scripts','pack'))//,gulp.parallel(   'manifest')))//,
 // 'scripts',
 // 'styles',
 // 'pages',
